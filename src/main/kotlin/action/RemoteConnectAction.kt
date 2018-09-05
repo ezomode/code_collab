d24064c04e2ec209b50c9a572bc00a31aaaf9e79
ezomode <ezomode@gmail.com>
2018-09-05 23:53:05 +0200

    add connect to remote session action

---
+++ src/main/kotlin/action/RemoteConnectAction.kt
@@ -0,0 +1,38 @@
+package action
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.ui.Messages
+import model.State
+import service.MainService
+import service.NetworkService
+
+class RemoteConnectAction : AnAction("Join Remote Session") {
+
+  var defaultAddressPort = "localhost:4444"
+
+  override fun actionPerformed(event: AnActionEvent) {
+    val project = event.getData(PlatformDataKeys.PROJECT)
+    val data = Messages.showInputDialog(project, "<ip_address>:<port>", "Connect to edit together", Messages.getInformationIcon(), defaultAddressPort, null)
+
+    data?.let {
+
+      val split = data.split(":").dropLastWhile(String::isEmpty)
+
+      if (split.size == 2) {
+        defaultAddressPort = data
+        NetworkService.getInstance().connect(split[0], split[1])
+      }
+    }
+
+    //		Messages.showMessageDialog(project, "Hello, " + txt + "!\n I am glad to see you.", "Information", Messages.getInformationIcon());
+  }
+
+  override fun update(e: AnActionEvent) {
+
+    val service = MainService.getInstance()
+
+    e.presentation.isVisible = service.state == State.IDLE
+  }
+}
