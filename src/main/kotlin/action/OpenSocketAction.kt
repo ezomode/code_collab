264961377d3218a556401a964946132a54370548
ezomode <ezomode@gmail.com>
2018-09-05 23:23:42 +0200

    open local socket action

---
+++ src/main/kotlin/action/OpenSocketAction.kt
@@ -0,0 +1,22 @@
+package action
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import model.State
+import service.MainService
+import service.NetworkService
+
+class OpenSocketAction : AnAction("Open Local Socket") {
+
+  override fun actionPerformed(event: AnActionEvent) {
+
+    NetworkService.getInstance().initServerSocket()
+  }
+
+  override fun update(e: AnActionEvent) {
+
+    val service = MainService.getInstance()
+
+    e.presentation.isVisible = service.state == State.IDLE
+  }
+}
