7f917fbac0fbba824bb7f9c77933853550d772a0
ezomode <ezomode@gmail.com>
2018-09-05 23:52:42 +0200

    add disconnect action

---
+++ src/main/kotlin/action/DisconnectAction.kt
@@ -0,0 +1,22 @@
+package action
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import model.State
+import service.MainService
+import service.NetworkService
+
+class DisconnectAction : AnAction("Disconnect") {
+
+  override fun actionPerformed(event: AnActionEvent) {
+
+    NetworkService.getInstance().disconnect()
+  }
+
+  override fun update(e: AnActionEvent) {
+
+    val service = MainService.getInstance()
+
+    e.presentation.isVisible = service.state != State.IDLE
+  }
+}
