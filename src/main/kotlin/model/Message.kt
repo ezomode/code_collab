6a73fd07dc7bdd6ea8c648d5784f471a68c75b2c
ezomode <ezomode@gmail.com>
2018-09-08 16:08:09 +0200

    set visible default values

--- src/main/kotlin/model/Message.kt
+++ src/main/kotlin/model/Message.kt
@@ -4,8 +4,8 @@ import com.beust.klaxon.Json
 import com.beust.klaxon.Klaxon
 
 data class Message(@Json val type: MessageType,
-                   @Json val projectName: String,
-                   @Json val path: String = "",
+                   @Json val projectName: String = "NA",
+                   @Json val path: String = "NA",
                    @Json val text: String = "") {
 
   fun json(): String {
