efcc4b725c194f5a8f939564d6a52b22a5a34f3a
ezomode <ezomode@gmail.com>
2018-09-08 13:59:48 +0200

    add project name field

--- src/main/kotlin/model/Message.kt
+++ src/main/kotlin/model/Message.kt
@@ -4,8 +4,9 @@ import com.beust.klaxon.Json
 import com.beust.klaxon.Klaxon
 
 data class Message(@Json val type: MessageType,
-                   @Json val payload: String = "",
-                   @Json val path: String = "") {
+                   @Json val projectName: String,
+                   @Json val path: String = "",
+                   @Json val text: String = "") {
 
   fun json(): String {
     return Klaxon().toJsonString(this)
